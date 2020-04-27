d48696f7c345180f6a1971a2e1db992f1c97eec2
Bashir <bashiralhanshali@yahoo.com>
2020-04-27 15:51:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+#require ("pry")
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |attribute|
+    attribute[1].each do |attributeOption|
+      attributeOption[1].map do |name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end 
+        if !(new_hash[name][attribute[0]])
+          new_hash[name][attribute[0]] = []
+        end 
+        new_hash[name][attribute[0]] << attributeOption[0].to_s
+      end 
+    end 
+  end 
+  new_hash
 end
