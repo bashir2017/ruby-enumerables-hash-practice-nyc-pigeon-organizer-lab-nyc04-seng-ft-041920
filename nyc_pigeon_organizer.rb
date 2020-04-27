14650c4ae8a80644e98eee1861b145dd878c476b
Bashir <bashiralhanshali@yahoo.com>
2020-04-27 15:37:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+#require ("pry")
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each do |attribute|
+    attribute[1].each do |attributeOption|
+      attributeOption[1].map do |name|
+        new_hash[name] = {}
+        new_hash[name][attribute[0]] = []
+        new_hash[name][attribute[0]] << attributeOption
+      end 
+    end 
+  end 
+  new_hash
 end
