379dc2aee133b2beadbde46f1db4e14393fa0c9c
Bashir <bashiralhanshali@yahoo.com>
2020-04-27 15:50:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,9 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute|
     attribute[1].each do |attributeOption|
       attributeOption[1].map do |name|
-        new_hash[name] = {}
+        if !new_hash[name]
+          new_hash[name] = {}
+        end 
         if !(new_hash[name][attribute[0]])
           new_hash[name][attribute[0]] = []
         end 
